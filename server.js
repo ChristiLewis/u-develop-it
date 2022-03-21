e6ea14159e4eb2a56b916f870021354cb45737db
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-03-21 08:00:24 -0700

    route tests working

--- server.js
+++ server.js
@@ -32,15 +32,53 @@ const db = mysql.createConnection(
 //     });
 //   });
 
-//QUERY THE DATABASE TO TEST THE CONNECTION
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// //QUERY THE DATABASE TO TEST THE CONNECTION
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+//  //ADD A CATCHALL ROUTE AFTER THE GET ROUTE TO HANDLE ERRONEOUS REQUEST NOT HANDLED BY THE APP
+//  app.use((req, res) => {
+//     res.status(404).end();
+//   });
+
+//GET ALL CANDIDATES
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
 });
 
- //ADD A CATCHALL ROUTE AFTER THE GET ROUTE TO HANDLE ERRONEOUS REQUEST NOT HANDLED BY THE APP
- app.use((req, res) => {
-    res.status(404).end();
+//SINGLE CANDIDATE
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
   });
+});
+
+//DELTE CANDIDATE
+
+
+//CREATE CANDIDATE
  
 // HERE AT THE BOTTOM OF THE FILE -ADD THE FUNCTION TO START THE EXPRESS.JS SERVER ON PORT 3001
 app.listen(PORT, () => {
